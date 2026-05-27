a6719cc3a5948f97610a6365ff9aa82cb57297ba
CHLOE MONASTIRSKY <49552251@ort>
2026-05-27 12:05:21 -0300

    terminamos los post

--- src/controllers/province-controller.js
+++ src/controllers/province-controller.js
@@ -5,3 +5,26 @@ router.get("/", async (req, res) => {
     res.json(provinces);
 
 });
+
+router.get("/:id", async (req, res) => {
+
+    const province = await service.getByIdAsync(req.params.id);
+
+    res.json(province);
+
+});
+
+router.post("/", async (req, res) => {
+
+    let province = await service.createAsync(req.body);
+
+    res.json(province);
+
+});
+router.post("/", async (req, res) => {
+
+    let province = await service.updateAsync(req.body);
+
+    res.json(province);
+
+});
